7900ed4daae6b969012f7d35fef2c4805d72eb55
xjvnet <32218875+xjvnet@users.noreply.github.com>
2020-11-30 17:51:26 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/project-openwrt/openwrt && cd openwrt
+git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/project-openwrt/openwrt && git clone lienol https://github.com/Lienol/openwrt-package && cd openwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a popd
